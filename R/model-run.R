2a8cb3bb11d5feef4b10d179a1a45b99784c5511
Lucas Godoy <lucasdac.godoy@gmail.com>
2025-01-31 10:28:06 -0800

    attempt to build a 'fit_drm' function

--- R/model-run.R
+++ R/model-run.R
@@ -1,29 +1,115 @@
 ##' @title Fit the dynamic range model.
 ##' @export
 ##' @family models
-##' @description Fit the DRM Stan model and return posterior summary.
-##' @return A data frame of posterior summaries.
-##' @param data_list a \code{list} created using the [make_data()] function.
-##' @param ... Passed on to the \code{sample()} method of CmdStan model objects:
-##'   <https://mc-stan.org/cmdstanr/reference/model-method-sample.html>
+##' @description Fit the DRM Stan model (this function is not stable yet and
+##'   have not been fully tested).
+##' @param .data A \code{data frame} containing the data for the model.
+##' @param y_col A \code{character} specifying the name of the column in `.data`
+##'   that contains the response variable.
+##' @param time_col A \code{character} specifying the name of the column in
+##'   `.data` that contains the time variable.
+##' @param site_col A \code{character} specifying the name of the column in
+##'   `.data` that contains the site variable.
+##' @param family a \code{character} specifying the family of the probability
+##'   distribution assumed for density. The options are: \itemize{ \item
+##'   \code{"lognormal1"} (default): log-normal with the usual parametrization;
+##'   \item \code{"lognormal2"}: log-normal parametrized in terms of its mean;
+##'   \item \code{"gamma"}: gamma parametrized in terms of its mean; \item
+##'   \code{"loglogistic"}: log-logistic parametrized in terms of its mean.}
+##' @param formula_zero A \code{formula} specifying the model for the zero
+##'   inflation component. Defaults to `~ 1` (intercept only).
+##' @param formula_rec A \code{formula} specifying the model for the recruitment
+##'   component. Defaults to `~ 1` (intercept only).
+##' @param formula_surv A \code{formula} specifying the model for the survival
+##'   component. If `NULL` (the default), no survival component is included.
+##' @param iter_warmup An \code{integer} specifying the number of warmup
+##'   iterations for the MCMC sampler. Defaults to 1000.
+##' @param iter_sampling An \code{integer} specifying the number of sampling
+##'   iterations for the MCMC sampler. Defaults to 1000.
+##' @param chains An \code{integer} specifying the number of MCMC chains.
+##'   Defaults to 4.
+##' @param parallel_chains An \code{integer} specifying the number of chains to
+##'   run in parallel. Defaults to 4.
+##' @param seed An \code{integer} specifying the random number seed.
+##' @param init A \code{character} specifying the initialization method.  Can be
+##'   "cmdstan_default" (the default) or "pathfinder".
+##' @param ... Passed on to the [make_data()] function used to build the input
+##'   \code{list} for our \code{cmdstanr} model.
+##' @return A \code{list} containing the MCMC draws and the model data.
+##'   Specifically: \itemize{ \item{\code{draws}: }{The MCMC draws from the
+##'   fitted model.}  \item{\code{data}: }{The data used to fit the model (as a
+##'   list).}  }
 ##' @seealso [make_data()]
 ##' @examples
 ##' if (instantiate::stan_cmdstan_exists()) {
 ##'   data(sum_fl)
-##'   make_data(y = sum_fl$y,
-##'             time = sum_fl$year,
-##'             site = sum_fl$patch,
-##'             n_ages = 8,
-##'             age_at_maturity = integer(0),
-##'             m = 0.25) |>
-##'   run_drm()
+##'     fit_drm(.data = sum_fl,
+##'             y_col = "y",
+##'             time_col = "time",
+##'             site_col = "site")$draws$summary()
 ##' }
 ##' @author lcgodoy
-run_drm <- function(data_list, ...) {
+fit_drm <- function(.data,
+                    y_col,
+                    time_col,
+                    site_col,
+                    family = "lognormal1",
+                    formula_zero = ~ 1,
+                    formula_rec = ~ 1,
+                    formula_surv = NULL,
+                    iter_warmup   = 1000,
+                    iter_sampling = 1000,
+                    chains = 4,
+                    parallel_chains = 4,
+                    seed,
+                    init = "cmdstan_default",
+                    ...) {
+  stopifnot(init %in% c("cmdstan_default", "pathfinder"))
+  x_t <- model.matrix(formula_zero, data = .data)
+  x_r <- model.matrix(formula_rec, data = .data)
+  if (is.null(formula_surv)) {
+    model_dat <- make_data(y = .data[[y_col]],
+                           time = .data[[time_col]],
+                           site = .data[[site_col]],
+                           family = family,
+                           x_t = x_t,
+                           x_r = x_r,
+                           ...)
+  } else {
+    x_m <- model.matrix(formula_surv, data = .data)
+    model_dat <- make_data(y = .data[[y_col]],
+                           time = .data[[time_col]],
+                           site = .data[[site_col]],
+                           family = family,
+                           x_t = x_t,
+                           x_r = x_r,
+                           x_m = x_m,
+                           ...)
+  }
   model <- instantiate::stan_package_model(
-    name = "drm",
-    package = "drmr"
-  )
-  fit <- model$sample(data = data_list, ...)
-  fit$summary()
+                            name = "drm",
+                            package = "drmr"
+                        )
+  if (init == "cmdstan_default") {
+    drm_init <- NULL
+  } else {
+    drm_init <-
+      model$pathfinder(data = model_dat,
+                       seed = seed,
+                       num_paths = chains,
+                       save_single_paths = TRUE,
+                       psis_resample = FALSE)
+  }
+  draws <- model$sample(data = model_dat,
+                        iter_warmup = iter_warmup,
+                        iter_sampling = iter_sampling,
+                        seed = seed,
+                        chains = chains,
+                        parallel_chains = parallel_chains,
+                        init = drm_init)
+  output <-
+    list(draws = "draws",
+         data  = "model_dat")
+
+  return(output)
 }
